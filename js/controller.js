54b75f082d1071e270d646e7fcc4bbfa2f613f61
WALTER INACIO DA SILVA FILHO <16527301@acd.puc-campinas.edu.br>
2017-03-31 19:44:11 -0300

    redirecionamento para testes

--- js/controller.js
+++ js/controller.js
@@ -3,12 +3,17 @@ var app = angular.module("app", ["ngRoute"]);
 app.config(["$routeProvider", function($routeProvider){
 	$routeProvider.
 	when("/",{templateUrl: "manutencao.html"}).
-	when("/prod",{templateUrl: "producao.html"}).
+	when("/prod",{
+		templateUrl: "producao.html",
+		controller: "myController",
+	}).
 	otherwise({redirectTo: "/"});
 
 }]);
 
 
 app.controller("myController", function($scope, $http, $location){
-	$location.path("/prod");
+	$(document).ready(function(){
+	  $('.parallax').parallax();
+	});
 });
